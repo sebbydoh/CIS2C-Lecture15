0f806e25bd9efc07ea35ba13234b589875212e4e
Encinia <sebastienencinia@my.whccd.edu>
2020-03-02 21:24:35 -0800

    Forgot to make iterative commits

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-let nameField = document.getElementById = "nameField";
+let textInput = document.getElementById("textInput");
+let colorInput = document.getElementById("colorInput");
+let form = document.querySelector("form");
+
+function submitFunction(event) {
+  let savedColor = colorInput.value;
+  let body = document.querySelector("body");
+
+  event.preventDefault();
+  console.log(colorInput.value);
+  if(colorInput.value != null){
+      body.style.backgroundColor = savedColor;
+  }
+  localStorage.setItem("localColor", savedColor);
+  let color = localStorage.getItem("localColor");
+  
+  alert("Hexadecimal color is " + color);
+
+}
+
+form.addEventListener("submit", submitFunction);
